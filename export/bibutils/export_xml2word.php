7aa7381e65a6cc63893dbfbf4171c81231d82826
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2007-03-21 22:35:26 +0000

    initial checkin (exports records to Word 2007 bibliography XML format)
    git-svn-id: https://refbase.svn.sourceforge.net/svnroot/refbase/branches/bleeding-edge@907 47a94560-c220-0410-a5a2-efdbd790cbb4

---
+++ export/bibutils/export_xml2word.php
@@ -0,0 +1,42 @@
+<?php
+	// Project:    Web Reference Database (refbase) <http://www.refbase.net>
+	// Copyright:  Matthias Steffens <mailto:refbase@extracts.de> and the file's
+	//             original author(s).
+	//
+	//             This code is distributed in the hope that it will be useful,
+	//             but WITHOUT ANY WARRANTY. Please see the GNU General Public
+	//             License for more details.
+	//
+	// File:       ./export/bibutils/export_xml2word.php
+	// Repository: $HeadURL$
+	// Author(s):  Matthias Steffens <mailto:refbase@extracts.de>
+	//
+	// Created:    21-Mar-07, 23:17
+	// Modified:   $Date$
+	//             $Author$
+	//             $Revision$
+
+	// This is an export format file (which must reside within the 'export/' sub-directory of your refbase root directory). It contains a version of the
+	// 'exportRecords()' function that outputs records according to the Word 2007 bibliography XML format (http://office.microsoft.com/word/).
+	// This function is basically a wrapper for the bibutils 'xml2word' command line tool (http://www.scripps.edu/~cdputnam/software/bibutils/bibutils.html).
+
+	// --------------------------------------------------------------------
+
+	// --- BEGIN EXPORT FORMAT ---
+
+	// Export found records in Word 2007 bibliography format:
+
+	// Requires the following packages (available under the GPL):
+	//    - bibutils <http://www.scripps.edu/~cdputnam/software/bibutils/bibutils.html>
+	//    - ActiveLink PHP XML Package <http://www.active-link.com/software/>
+
+	function exportRecords($result, $rowOffset, $showRows, $exportStylesheet, $displayType)
+	{
+		// function 'exportBibutils()' is defined in 'execute.inc.php'
+		return exportBibutils($result,"xml2word");
+	}
+
+	// --- END EXPORT FORMAT ---
+
+	// --------------------------------------------------------------------
+?>
