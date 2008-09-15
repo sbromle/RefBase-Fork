3a20c262300e3530838c4fd26b71f960d26c0651
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2008-09-15 13:32:39 +0000

    * added standard refbase file header * updated function 'displayHTMLhead()' to match the new version of that   function in 'includes/header.inc.php'
    git-svn-id: https://refbase.svn.sourceforge.net/svnroot/refbase/branches/bleeding-edge@1223 47a94560-c220-0410-a5a2-efdbd790cbb4

--- contrib/skins/mediawiki-monobook/header.inc.php
+++ contrib/skins/mediawiki-monobook/header.inc.php
@@ -1,35 +1,58 @@
 <?php
-	// Copyright:  Richard Karnesky <mailto:karnesky@gmail.com>
-	//             This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
-	//             Please see the GNU General Public License for more details.
+	// Project:    Web Reference Database (refbase) <http://www.refbase.net>
+	// Copyright:  Matthias Steffens <mailto:refbase@extracts.de> and the file's
+	//             original author(s).
+	//
+	//             This code is distributed in the hope that it will be useful,
+	//             but WITHOUT ANY WARRANTY. Please see the GNU General Public
+	//             License for more details.
+	//
 	// File:       ./includes/header.inc.php
+	// Repository: $HeadURL$
+	// Author(s):  Richard Karnesky <mailto:karnesky@gmail.com>
+	//
+	// Created:    31-Oct-06, 00:28
+	// Modified:   $Date$
+	//             $Author$
+	//             $Revision$
+
+	// This is the header include file.
+	// It contains functions that provide the HTML header
+	// as well as the visible header that gets displayed on every page.
 
 	// --------------------------------------------------------------------
 
 	// Inserts the HTML <head>...</head> block as well as the initial <body> tag:
+	// 
+	// TODO: include OpenSearch elements in HTML header
+	//       (see examples at <http://www.opensearch.org/Specifications/OpenSearch/1.1#Response_metadata_in_HTML.2FXHTML>)
 	function displayHTMLhead($pageTitle, $metaRobots, $metaDescription, $additionalMeta, $includeJavaScript, $includeJavaScriptFile, $viewType, $rssURLArray)
 	{
-		global $contentTypeCharset; // these variables are specified in 'ini.inc.php' 
+		global $officialDatabaseName; // these variables are defined in 'ini.inc.php'
+		global $contentTypeCharset;
 		global $defaultStyleSheet;
 		global $printStyleSheet;
 		global $mobileStyleSheet;
+		global $defaultLanguage;
 		global $databaseBaseURL;
+		global $databaseKeywords;
+		global $defaultFeedFormat;
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 		"http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
+<head profile="http://a9.com/-/spec/opensearch/1.1/">
 	<title><?php echo $pageTitle; ?></title>
 	<meta name="date" content="<?php echo date('d-M-y'); ?>">
 	<meta name="robots" content="<?php echo $metaRobots; ?>">
 	<meta name="description" lang="en" content="<?php echo $metaDescription; ?>">
-	<meta name="keywords" lang="en" content="science academic literature scientific references search citation web database mysql php"><?php
+	<meta name="keywords" lang="en" content="<?php echo $databaseKeywords; ?>"><?php
 
 		if (!empty($additionalMeta))
 			echo $additionalMeta;
 ?>
 
-	<meta http-equiv="content-language" content="en">
+	<meta http-equiv="content-language" content="<?php echo $defaultLanguage; ?>">
 	<meta http-equiv="content-type" content="text/html; charset=<?php echo $contentTypeCharset; ?>">
 	<meta http-equiv="Content-Style-Type" content="text/css"><?php
 
@@ -38,6 +61,12 @@
 ?>
 
 	<link rel="stylesheet" href="<?php echo $printStyleSheet; ?>" type="text/css" title="CSS Definition"><?php
+		}
+		elseif (eregi("^Mobile$", $viewType))
+		{
+?>
+
+	<link rel="stylesheet" href="<?php echo $mobileStyleSheet; ?>" type="text/css" title="CSS Definition"><?php
 		}
 		else
 		{
@@ -50,78 +79,36 @@
 		{
 			foreach ($rssURLArray as $rssURL)
 			{
+				if ($defaultFeedFormat == "Atom XML")
+					$feedContentType = "application/atom+xml";
+				else // RSS XML
+					$feedContentType = "application/rss+xml";
+
 			// ...include a link tag pointing to a dynamic RSS feed for the current query:
 ?>
 
-	<link rel="alternate" type="application/rss+xml" href="<?php echo $rssURL['href']; ?>" title="<?php echo $rssURL['title']; ?>"><?php
+	<link rel="alternate" type="<?php echo $feedContentType; ?>" href="<?php echo $databaseBaseURL . $rssURL['href']; ?>" title="<?php echo $rssURL['title']; ?>"><?php
 			}
 		}
 ?>
 
-	<link rel="unapi-server" type="application/xml" title="unAPI" href="<?php echo $databaseBaseURL; ?>unapi.php"><?php
-
-		if (!empty($includeJavaScriptFile))
-		{
-?>
-
-	<script language="JavaScript" type="text/javascript" src="<?php echo $includeJavaScriptFile; ?>">
-		</script><?php
-		}
+	<link rel="unapi-server" type="application/xml" title="unAPI" href="<?php echo $databaseBaseURL; ?>unapi.php">
+	<link rel="search" type="application/opensearchdescription+xml" title="<?php echo encodeHTML($officialDatabaseName); ?>" href="<?php echo $databaseBaseURL; ?>opensearch.php?operation=explain"><?php
 
 		if ($includeJavaScript)
 		{
+			// ...include a script tag to include common JavaScript functions:
 ?>
 
-	<script language="JavaScript" type="text/javascript">
-		function checkall(val, formpart) {
-			var x = 0;
-			while(document.queryResults.elements[x]) {
-				if(document.queryResults.elements[x].name == formpart) {
-					document.queryResults.elements[x].checked = val;
-				}
-				x++;
-			}
-			toggleRadio('allRecs', 'selRecs', val);
+	<script language="JavaScript" type="text/javascript" src="javascript/common.js"></script><?php
 		}
-		function toggleVisibility(id, imgid, txtid, txt) {
-			var e = document.getElementById(id);
-			var i = document.getElementById(imgid);
-			var t = document.getElementById(txtid);
-			if(e.style.display == 'block') {
-				e.style.display = 'none';
-				i.src = 'img/closed.gif';
-				t.innerHTML = txt;
-			}
-			else {
-				e.style.display = 'block';
-				i.src = 'img/open.gif';
-				t.innerHTML = '';
-			}
-		}
-		function toggleRadio(id1, id2, val) {
-			document.getElementById(id1).checked = !(val);
-			document.getElementById(id2).checked = val;
-		}
-		function updateAllRecs() {
-			var val=!eval("document.getElementById('allRecs').checked");
-			var x=0;
-			var checked=0;
-			while(document.queryResults.elements[x]) {
-				if(document.queryResults.elements[x].name == "marked[]") {
-					if (eval("document.queryResults.elements[x].checked")) {
-						checked++;
-					}
-				}
-				x++;
-			}
-			if (checked>0) {
-				val=true;
-			} else {
-				val=false;
-			}
-			toggleRadio('allRecs', 'selRecs', val);
-		}
-	</script><?php
+
+		if (!empty($includeJavaScriptFile))
+		{
+			// ...include another script tag to include additional JavaScript functions:
+?>
+
+	<script language="JavaScript" type="text/javascript" src="<?php echo $includeJavaScriptFile; ?>"></script><?php
 		}
 ?>
 
