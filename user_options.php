79923334aacade648813c91e51b632152e56c446
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2008-09-15 14:05:27 +0000

    we now use variable '$availableMainFields' (from 'ini.inc.php') to define the list of fields that are available in the "main fields" selector of the user's account options page
    git-svn-id: https://refbase.svn.sourceforge.net/svnroot/refbase/branches/bleeding-edge@1235 47a94560-c220-0410-a5a2-efdbd790cbb4

--- user_options.php
+++ user_options.php
@@ -247,62 +247,14 @@
 	else // if ($loginEmail != $adminLoginEmail) // if a normal user is logged in
 		$selectListIdentifier = "Show";
 
+	// Map MySQL field names to localized column names:
+	$fieldNamesArray = mapFieldNames(true); // function 'mapFieldNames()' is defined in 'include.inc.php'
+	$mainFieldsArray = array();
+
 	// Define fields that can be designated as "main fields":
-	$mainFieldsArray = array("author"              => $loc["DropDownFieldName_Author"],
-//	                         "address"             => $loc["DropDownFieldName_Address"],
-//	                         "corporate_author"    => $loc["DropDownFieldName_CorporateAuthor"],
-//	                         "thesis"              => $loc["DropDownFieldName_Thesis"],
-	                         "title"               => $loc["DropDownFieldName_Title"],
-//	                         "orig_title"          => $loc["DropDownFieldName_OrigTitle"],
-	                         "year"                => $loc["DropDownFieldName_Year"],
-	                         "publication"         => $loc["DropDownFieldName_Publication"],
-	                         "abbrev_journal"      => $loc["DropDownFieldName_AbbrevJournal"],
-	                         "editor"              => $loc["DropDownFieldName_Editor"],
-//	                         "volume"              => $loc["DropDownFieldName_Volume"],
-//	                         "issue"               => $loc["DropDownFieldName_Issue"],
-//	                         "pages"               => $loc["DropDownFieldName_Pages"],
-	                         "series_title"        => $loc["DropDownFieldName_SeriesTitle"],
-	                         "abbrev_series_title" => $loc["DropDownFieldName_AbbrevSeriesTitle"],
-//	                         "series_editor"       => $loc["DropDownFieldName_SeriesEditor"],
-//	                         "series_volume"       => $loc["DropDownFieldName_SeriesVolume"],
-//	                         "series_issue"        => $loc["DropDownFieldName_SeriesIssue"],
-//	                         "publisher"           => $loc["DropDownFieldName_Publisher"],
-//	                         "place"               => $loc["DropDownFieldName_Place"],
-//	                         "edition"             => $loc["DropDownFieldName_Edition"],
-//	                         "medium"              => $loc["DropDownFieldName_Medium"],
-//	                         "issn"                => $loc["DropDownFieldName_Issn"],
-//	                         "isbn"                => $loc["DropDownFieldName_Isbn"],
-//	                         "language"            => $loc["DropDownFieldName_Language"],
-//	                         "summary_language"    => $loc["DropDownFieldName_SummaryLanguage"],
-	                         "keywords"            => $loc["DropDownFieldName_Keywords"],
-	                         "abstract"            => $loc["DropDownFieldName_Abstract"],
-	                         "area"                => $loc["DropDownFieldName_Area"],
-//	                         "expedition"          => $loc["DropDownFieldName_Expedition"],
-//	                         "conference"          => $loc["DropDownFieldName_Conference"],
-//	                         "doi"                 => $loc["DropDownFieldName_Doi"],
-//	                         "url"                 => $loc["DropDownFieldName_Url"],
-//	                         "file"                => $loc["DropDownFieldName_File"],
-	                         "notes"               => $loc["DropDownFieldName_Notes"],
-//	                         "location"            => $loc["DropDownFieldName_Location"],
-	                         "call_number"         => $loc["DropDownFieldName_CallNumber"],
-	                         "serial"              => $loc["DropDownFieldName_Serial"],
-//	                         "type"                => $loc["DropDownFieldName_Type"],
-//	                         "approved"            => $loc["DropDownFieldName_Approved"],
-//	                         "created_date"        => $loc["DropDownFieldName_CreatedDate"],
-//	                         "created_time"        => $loc["DropDownFieldName_CreatedTime"],
-//	                         "created_by"          => $loc["DropDownFieldName_CreatedBy"],
-//	                         "modified_date"       => $loc["DropDownFieldName_ModifiedDate"],
-//	                         "modified_time"       => $loc["DropDownFieldName_ModifiedTime"],
-//	                         "modified_by"         => $loc["DropDownFieldName_ModifiedBy"],
-//	                         "marked"              => $loc["DropDownFieldName_Marked"],
-//	                         "copy"                => $loc["DropDownFieldName_Copy"],
-//	                         "selected"            => $loc["DropDownFieldName_Selected"],
-//	                         "user_keys"           => $loc["DropDownFieldName_UserKeys"],
-//	                         "user_notes"          => $loc["DropDownFieldName_UserNotes"],
-//	                         "user_file"           => $loc["DropDownFieldName_UserFile"],
-//	                         "user_groups"         => $loc["DropDownFieldName_UserGroups"],
-	                         "cite_key"            => $loc["DropDownFieldName_CiteKey"],
-	                        );
+	foreach ($availableMainFields as $field) // variable '$availableMainFields' is defined in 'ini.inc.php'
+		if (isset($fieldNamesArray[$field]))
+			$mainFieldsArray[$field] = $fieldNamesArray[$field];
 
 	// Build properly formatted <option> tag elements from array items given in '$mainFieldsArray':
 	$mainFieldsOptionTags = buildSelectMenuOptions($mainFieldsArray, "", "\t\t\t", true); // function 'buildSelectMenuOptions()' is defined in 'include.inc.php'
